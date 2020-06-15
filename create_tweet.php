113fe3aa1ec8485f5bfbe109c89eb2b2fb0d7ade
saminyeaser <saminyeaser1@gmail.com>
2020-06-15 12:43:46 +0600

    twitter can be created

---
+++ create_tweet.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'dbconnect.php';
+session_start();
+if(!isset($_POST['body'])){
+    exit();
+}
+$user_id = $_SESSION['user'];
+$userData = $db->users->findOne(array('_id'=>$user_id));
+$body = substr($_POST['body'],140);
+$date = date('Y-m-d H:i:s');
+
+$db->tweets->insertOne(array(
+    'authorID'=>$user_id,
+    'authorName'=>$userData['username'],
+    'body'=>$body,
+    'created'=>date
+
+));
+header('Location: home.php');
+?>
