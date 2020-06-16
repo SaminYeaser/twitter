dcb99cffa1ff1f0fd20b10c6a02d0e571ec9b05d
saminyeaser <saminyeaser1@gmail.com>
2020-06-16 09:00:45 +0600

    bootstrap added skiliteon

--- header.php
+++ header.php
@@ -1,7 +1,31 @@
-<div>
-    <span><h1>Welcome to twitter <?php echo $userData['username']?></h1></span><br>
-    <a href="home.php">Home</a>
-    <a href="profile.php?id=<?php echo $_SESSION['user']?>">View Profile</a>
-    <a href="userlist.php">View Users</a>
-    <a href="logout.php">Logout</a>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <span><h1>Welcome to twitter <?php echo $userData['username']?></h1></span><br>
+                <a href="home.php">Home</a>
+                <a href="profile.php?id=<?php echo $_SESSION['user']?>">View Profile</a>
+                <a href="userlist.php">View Users</a>
+                <a href="logout.php">Logout</a>
+            </div>
+        </div>
+    </div>
+
 </div>
+</body>
+</html>
